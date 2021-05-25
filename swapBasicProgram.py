d53359e2809d3958de735bcd9b2ee80b88d2c55d
Saptorsi-Mondal-230198 <saptorsimonit17@student.mes.ac.in>
2021-05-25 19:42:55 +0530

    Adding Basic Swap Program in Python

---
+++ swapBasicProgram.py
@@ -0,0 +1,9 @@
+a, b = 10, 100
+print("Before Swapping:--")
+print("a = ",a)
+print("b = ",b)
+print("---------------------------------------------------------------------------------------------------")
+a, b = b, a
+print("After Swapping:--")
+print("a = ",a)
+print("b = ",b)
