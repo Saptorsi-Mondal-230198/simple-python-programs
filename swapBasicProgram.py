67dbb8d8967f309c42dd7ef8bd748d52a4d50f3f
Saptorsi-Mondal-230198 <84730796+Saptorsi-Mondal-230198@users.noreply.github.com>
2021-05-25 19:56:19 +0530

    Update swapBasicProgram.py

--- swapBasicProgram.py
+++ swapBasicProgram.py
@@ -1,9 +1,4 @@
 a, b = 10, 100
-print("Before Swapping:--")
-print("a = ",a)
-print("b = ",b)
-print("---------------------------------------------------------------------------------------------------")
 a, b = b, a
-print("After Swapping:--")
-print("a = ",a)
-print("b = ",b)
+print(a)
+print(b)
